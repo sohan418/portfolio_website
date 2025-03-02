6773ca58698a250630e206da2fb11d1858584d07
sde1 <sde1@valuenable.in>
2025-03-02 10:36:07 +0530

    add button arrow bottom to top

--- src/components/ContactUs/Contact.js
+++ src/components/ContactUs/Contact.js
@@ -7,20 +7,17 @@ const Contact = () => {
       <h2 className="contact-title">Get in Touch</h2>
       <p className="contact-text">I’d love to hear from you! Whether it’s about art commissions, freelance development, or exciting collaborations — reach out!</p>
       <div className="contact-links">
-        <a href="mailto:your.email@example.com" target="_blank" rel="noopener noreferrer" className="contact-icon">
+        <a href="mailto:sohanbisht418@gmail.com" target="_blank" rel="noopener noreferrer" className="contact-icon">
           <FaEnvelope />
         </a>
-        <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer" className="contact-icon">
+        <a href="https://www.linkedin.com/in/sohan-singh-bisht-bb5788279?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" rel="noopener noreferrer" className="contact-icon">
           <FaLinkedin />
         </a>
-        <a href="https://github.com/yourprofile" target="_blank" rel="noopener noreferrer" className="contact-icon">
+        <a href="https://github.com/sohan418/" target="_blank" rel="noopener noreferrer" className="contact-icon">
           <FaGithub />
         </a>
-        <a href="https://instagram.com/yourartprofile" target="_blank" rel="noopener noreferrer" className="contact-icon">
+        <a href="https://www.instagram.com/sohanuk_01?igsh=YWZ1b3M4aHpzcWV5" target="_blank" rel="noopener noreferrer" className="contact-icon">
           <FaInstagram />
-        </a>
-        <a href="https://twitter.com/yourprofile" target="_blank" rel="noopener noreferrer" className="contact-icon">
-          <FaTwitter />
         </a>
       </div>
     </div>
